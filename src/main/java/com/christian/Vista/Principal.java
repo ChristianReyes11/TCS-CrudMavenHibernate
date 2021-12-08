55cc00c2ed3a836bd44347a0f2515476a74bfdd7
FERNANDO-SOPORTE <christianamaro2000@gmail.com>
2021-12-08 11:13:46 -0600

    modified com.christian.Vista.Principal and pom.xml

--- src/main/java/com/christian/Vista/Principal.java
+++ src/main/java/com/christian/Vista/Principal.java
@@ -83,11 +83,13 @@ public class Principal extends javax.swing.JFrame {
 
     private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
         // TODO add your handling code here:
+        System.exit(0);
     }//GEN-LAST:event_jMenuItem1ActionPerformed
 
     private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
         // TODO add your handling code here:
         FormularioGUI gui = FactoryGUI.create(FactoryGUI.TypeGUI.EMPLEADO);
+        gui.show();
         jDesktopPane1.add(gui);
     }//GEN-LAST:event_jMenuItem2ActionPerformed
 
